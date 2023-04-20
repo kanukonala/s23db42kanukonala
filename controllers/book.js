3d60cb9bf85bf2df0fe4c4ff9e25328edbcef435
Yagna Venkata Teja Kanukonala <123043536+kanukonala@users.noreply.github.com>
2023-04-19 22:48:28 -0500

    good

--- controllers/book.js
+++ controllers/book.js
@@ -11,8 +11,8 @@ exports.book_list = async function(req, res) {
     }
     };
     
-// for a specific Costume.
-// for a specific Costume.
+// for a specific book.
+// for a specific book.
 exports.book_detail = async function(req, res) {
     console.log("detail" + req.params.id)
     try {
@@ -24,14 +24,14 @@ exports.book_detail = async function(req, res) {
     }
     };
     
-// Handle Costume create on POST.
+// Handle book create on POST.
 exports.book_create_post = async function(req, res) {
     console.log(req.body)
     let document = new book();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"book_type":"goat", "cost":12, "size":"large"}
     document.book_name = req.body.book_name;
     document.year_published = req.body.year_published;
     document.author = req.body.author;
@@ -45,11 +45,20 @@ exports.book_create_post = async function(req, res) {
     }
     };
     
-// Handle Costume delete form on DELETE.
-exports.book_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: book delete DELETE ' + req.params.id);
-};
-// Handle Costume update form on PUT.
+    // Handle book delete on DELETE.
+    exports.book_delete = async function(req, res) {
+        console.log("delete " + req.params.id)
+        try {
+        result = await book.findByIdAndDelete( req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+        } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+        }
+        };
+    
+// Handle book update form on PUT.
 exports.book_update_put = async function(req, res) {
 console.log(`update on id ${req.params.id} with body
 ${JSON.stringify(req.body)}`)
@@ -88,5 +97,63 @@ exports.book_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+
+    // Handle a show one view with id specified by query
+exports.book_view_one_Page = async function(req, res) {
+console.log("single view for id " + req.query.id)
+try{
+result = await book.findById( req.query.id)
+res.render('bookdetail',
+{ title: 'book Detail', toShow: result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+// Handle building the view for creating a book.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.book_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('bookcreate', { title: 'book Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+// Handle building the view for updating a book.
+// query provides the id
+exports.book_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await book.findById(req.query.id)
+    res.render('bookupdate', { title: 'book Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+   
+// Handle a delete one view with id from query
+exports.book_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await book.findById(req.query.id)
+    res.render('bookdelete', { title: 'book Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
+    
   
     
--- routes/book.js
+++ routes/book.js
@@ -7,8 +7,23 @@ router.get('/', function(req, res, next) {
   res.render('book', { title: 'Search Results Book' });
 
 });*/
+
 router.get('/', book_controlers.book_view_all_Page );
+/* GET detail costume page */
+router.get('/detail', book_controlers.book_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', book_controlers.book_create_Page);
+
+/* GET create update page */
+router.get('/update', book_controlers.book_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', book_controlers.book_delete_Page);
+
+
 // for a specific Costume.
 // GET request for one costume.
 //router.get('/book/:id', book_controlers.book_detail);
 module.exports = router;
+
