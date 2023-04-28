91ecdc70623fe3a515c6536a7cfa5ec0b618f74a
Yagna Venkata Teja Kanukonala <123043536+kanukonala@users.noreply.github.com>
2023-04-27 23:57:50 -0500

    Done

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ mongoose = require('mongoose');
 mongoose.connect(connectionString,
 {useNewUrlParser: true,
 useUnifiedTopology: true});
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -38,10 +43,6 @@ var book = require("./models/book");
 // passport config
 // Use the existing connection
 // The Account model
-var Account =require('./models/account');
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
 
 
 
@@ -56,6 +57,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -65,13 +76,25 @@ app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
 
-app.use(require('express-session')({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session());
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 //We can seed the collection if needed on server start
 async function recreateDB(){
@@ -102,27 +125,6 @@ instance3.save().then( () => {
 }
 let reseed = true;
 if (reseed) { recreateDB();}
-
-
-
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 
 
 module.exports = app;
--- routes/book.js
+++ routes/book.js
@@ -1,26 +1,28 @@
 var express = require('express');
 const book_controlers= require('../controllers/book');
 var router = express.Router();
+const secured = (req, res, next) => {
+  if (req.user){
+  return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+  }
 
-/* GET home page. 
-router.get('/', function(req, res, next) {
-  res.render('book', { title: 'Search Results Book' });
-
-});*/
-
+/* GET home page. */
 router.get('/', book_controlers.book_view_all_Page );
-/* GET detail costume page */
-router.get('/detail', book_controlers.book_view_one_Page);
 
-/* GET create costume page */
-router.get('/create', book_controlers.book_create_Page);
+/* GET detail book page */
+router.get('/detail', secured, book_controlers.book_view_one_Page);
+
+/* GET create book page */
+router.get('/create', secured, book_controlers.book_create_Page);
 
 /* GET create update page */
-router.get('/update', book_controlers.book_update_Page);
+router.get('/update', secured, book_controlers.book_update_Page);
 
-/* GET delete costume page */
-router.get('/delete', book_controlers.book_delete_Page);
-
+/* GET delete book page */
+router.get('/delete', secured, book_controlers.book_delete_Page);
 
 // for a specific Costume.
 // GET request for one costume.
